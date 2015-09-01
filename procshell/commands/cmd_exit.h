3a8b9a7762d5d18d3fa0fa8c57e1e00947e099f8
Neta Zmora <neta.zmora@gmail.com>
2015-09-01 23:43:17 +0300

    Added exit command

---
+++ procshell/commands/cmd_exit.h
@@ -0,0 +1,18 @@
+#ifndef __EXIT_CMD_H
+#define __EXIT_CMD_H
+
+#include "../terminal.h"
+#include "../commands.h"
+
+class ExitCmd : public ShellCommand {
+    Terminal &mTerm;
+    bool &mExitFlag;
+public:
+    ExitCmd(Terminal &term, bool &exit) : mTerm(term), mExitFlag(exit) { exit = false; }
+    const char* name() const { return "exit"; }
+    void handleCommand(const std::vector<std::string> &args) {
+        mExitFlag = true;
+    }
+};
+
+#endif // __EXIT_CMD_H
