c61ca3d44d03b632a8f06592b3de48a6ed2651fd
Neta Zmora <neta.zmora@gmail.com>
2015-09-01 23:42:26 +0300

    Added history command (missing in initial commit to github)

---
+++ procshell/commands/cmd_history.cpp
@@ -0,0 +1,10 @@
+#include "cmd_history.h"
+
+void HistoryCmd::handleCommand(const std::vector<std::string> &args) {
+    std::string cmd;
+    size_t i = 0;
+    // traverse the history list and print the command strings
+    while (mHistory.getCmdString(i++, cmd))
+        mTerm.puts(cmd.c_str(), true);
+}
+
---
+++ procshell/commands/cmd_history.h
@@ -0,0 +1,22 @@
+#ifndef __CMD_HISTORY_H
+#define __CMD_HISTORY_H
+
+#include <vector>
+#include "../terminal.h"
+#include "../history.h"
+#include "../commands.h"
+
+/*
+* This sample command provides access to the History from the command-line
+*/
+
+class HistoryCmd : public ShellCommand {
+    Terminal &mTerm;
+    History &mHistory;
+public:
+    HistoryCmd(Terminal &term, History &history) : mTerm(term), mHistory(history) {}
+    const char* name() const { return "history"; }
+    void handleCommand(const std::vector<std::string> &args);
+};
+
+#endif // __CMD_HISTORY_H
